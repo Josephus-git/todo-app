18c5ba02ad627e2e8581a4724d12edb8efa247f8
josephus <otejerejosephus@gmail.com>
2025-08-07 15:21:54 +0100

    added input for tasks

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,11 @@
 export function TodoInput() {
     return (
-        <div></div>
+        <div className="input-container">
+            <input placeholder="Add task"/>
+            <button>
+                <i class="fa-solid fa-plus"></i>
+            </button>
+
+        </div>
     )
 }
