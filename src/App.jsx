3966d2e1020d2ad9d9db28879852bc3d4f22a1de
josephus <otejerejosephus@gmail.com>
2025-08-07 12:42:37 +0100

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import { TodoList } from "./components/TodoList"
 
 
 function App() {
+
+  const todos = [
+    {input: 'Hello! Add your first todo!', complete: true },
+    {input: 'Get the groceries!', complete: false },
+    {input: 'Learn how to web design', complete: false },
+    {input: 'Say hi to gran gran', complete: true },
+  ]
+
   let x = 3
 
   return (
-    <div>
-
-      <Header />
-      <Tabs />
-      <TodoList />
+    <>
+      <Header todos={todos}/>
+      <Tabs todos={todos}/>
+      <TodoList todos={todos}/>
       <TodoInput />
-    </div>
+    </>
   )
 }
 
